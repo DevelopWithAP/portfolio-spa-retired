2d1de258652ce0927b8238e69bd2a5be363912f3
DevelopWithAP <apostolisparga@gmail.com>
2022-12-28 15:56:57 +0200

    Started dark mode. Must improve structure

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -9,9 +9,9 @@ type LayoutProps = {
 
 const Layout = ({ introduction, children, footer }: LayoutProps) => {
     return (
-        <div className='container mx-auto'>
+        <div className='container mx-auto border rounded px-2 py-2 my-1'>
             {introduction}
-            {<Content />}
+            {children = <Content/>}
             {footer}
         </div>
     );
